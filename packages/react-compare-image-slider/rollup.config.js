5fc9c60ae5c91df7bc08358b19e68a68a401fafa
johnwalley <john@walley.org.uk>
2020-07-02 14:19:00 +0100

    build: output both umd and es

--- packages/react-compare-image-slider/rollup.config.js
+++ packages/react-compare-image-slider/rollup.config.js
@@ -4,13 +4,21 @@ import typescript from "rollup-plugin-typescript2";
 export default {
   input: ["src/react-compare-image-slider.tsx"],
   external: ["react"],
-  output: {
-    name: "ReactCompareImageSlider",
-    file: "build/index.js",
-    format: "umd",
-    sourcemap: true,
-    globals: { react: "React" },
-  },
+  output: [
+    {
+      name: "ReactCompareImageSlider",
+      file: "build/index.umd.js",
+      format: "umd",
+      sourcemap: true,
+      globals: { react: "React" },
+    },
+    {
+      file: "build/index.js",
+      format: "es",
+      sourcemap: true,
+      globals: { react: "React" },
+    },
+  ],
   // eslint-disable-next-line no-undef
   plugins: [
     resolve(),
